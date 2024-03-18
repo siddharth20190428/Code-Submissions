e0bea5827d0bcd04f1159c0377527d9f2c95d395
Siddharth <siddharth201820@gmail.com>
2024-03-18 16:10:15 +0530

    created mysql connection and get, post request for adding and retriving the data

--- server/index.js
+++ server/index.js
@@ -1,17 +1,53 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mysql = require("mysql");
 
 const app = express();
-
 const corsOptions = {
-  origin: "http://localhost:8080/",
+  origin: "http://localhost:5000/",
 };
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Password@123",
+  database: "DB4",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL: " + err.stack);
+    return;
+  }
+  console.log("Connected to MySQL");
+});
+
+app.get("/submissions/", (req, res) => {
+  connection.query("SELECT * FROM submissions", (err, results) => {
+    if (err) {
+      console.error("Error executing query: " + err.stack);
+      return res.status(500).send("Error executing query");
+    }
+    // Render results or do something with them
+    res.json(results);
+  });
+});
+
+app.post("/submissions/create/", (req, res) => {
+  const data = req.body;
+  const query = `INSERT INTO submissions (username, language, stdin, stdout, source_code) VALUES ('${data.username}', '${data.language}', '${data.stdin}', '${data.stdout}', '${data.source_code}')`;
+
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error("Error executing query: " + err.stack);
+      return res.status(500).send("Error executing query");
+    }
+    // Render results or do something with them
+    res.json(results);
+  });
 });
 
 app.listen(5000, () => console.log("Server listening on PORT 5000"));
